4dcac62def0dffe974fd806e43bc977deee05f02
Obitope Eniola Nathaniel <obitopeeniola@gamil.com>
2024-01-29 12:35:20 +0100

    Initial commit

---
+++ project/fuction.py
@@ -0,0 +1,15 @@
+FiLEPATH = "todos.txt"
+
+
+def read_todo(filepath=FiLEPATH):
+    """ Read a text and return the list of
+    to-do items.
+    """
+    with open(filepath, "r") as read_file:
+        return read_file.readlines()
+
+
+def write_todo(todos_agr, filepath=FiLEPATH):
+    """ Write the to_do items list in the test file. """
+    with open(filepath, "w") as write_file:
+        write_file.writelines(todos_agr)

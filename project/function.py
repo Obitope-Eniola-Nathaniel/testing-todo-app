93137b5f75d927b0b5de857e528f1d604f09f2a9
Obitope Eniola Nathaniel <obitopeeniola@gamil.com>
2024-02-01 02:06:19 +0100

    improve 1 GUI

--- project/function.py
+++ project/function.py
@@ -1,7 +1,7 @@
-FiLEPATH = "todos.txt"
+FILEPATH = "todos.txt"
 
 
-def read_todo(filepath=FiLEPATH):
+def read_todo(filepath=FILEPATH):
     """ Read a text and return the list of
     to-do items.
     """
@@ -9,7 +9,7 @@ def read_todo(filepath=FiLEPATH):
         return read_file.readlines()
 
 
-def write_todo(todos_agr, filepath=FiLEPATH):
+def write_todo(todos_agr, filepath=FILEPATH):
     """ Write the to_do items list in the test file. """
     with open(filepath, "w") as write_file:
         write_file.writelines(todos_agr)
